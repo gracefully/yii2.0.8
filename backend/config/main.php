7392f877a6739b6b9bfc8b7de9309e16f3533cd0
huyangzhao <huyangzhao@sohu-inc.com>
2016-05-25 21:36:52 +0800

    m 增加yii2-admin模块

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,14 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    "modules" => [
+        "admin" => [
+            "class" => 'mdm\admin\Module',
+        ],
+    ],
+    "aliases" => [
+        "@mdm/admin" => "@vendor/mdmsoft/yii2-admin",
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
@@ -35,6 +42,10 @@ return [
             'rules' => [
             ],
         ],
+        "authManager" => [
+            "class" => 'yii\rbac\DbManager', //这里记得用单引号而不是双引号
+            "defaultRoles" => ["guest"],
+        ],
 //        "view" => [
 //            "theme" => [
 //                "pathMap" => [
@@ -42,6 +53,28 @@ return [
 //                ],
 //            ],
 //        ],
+    ],
+    'as access' => [
+        //ACF肯定是要加的，因为粗心导致该配置漏掉了，很是抱歉
+        'class' => 'mdm\admin\components\AccessControl',
+        'allowActions' => [
+            //这里是允许访问的action
+            //controller/action
+            'admin/route/index',
+            'admin/route/assign',
+            'admin/route/remove',
+            'debug/default/toolbar',
+            'admin/permission/index',
+            'admin/permission/create',
+            'admin/user/index',
+            'admin/role/index',
+            'admin/assignment/index',
+            'admin/assignment/view',
+            'admin/menu/index',
+            'admin/role/create',
+            'site/logout',
+            'site/index',
+        ]
     ],
     'params' => $params,
 ];
